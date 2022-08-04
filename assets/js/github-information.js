c99ef5052db4378519231046e297f1d2a0cdd20b
DOdrums <dirkornee@hotmail.com>
2022-08-04 13:59:55 +0000

    add js to displya github api and replace greay and loader images

---
+++ assets/js/github-information.js
@@ -0,0 +1,18 @@
+function fetchGitHubInformation(event) {
+
+    var username = $("#gh-username").val();
+    if (!username) {
+        $('#gh-user-data').html("<h2>Please enter a GitHub username</h2>");
+        return;
+    }
+   
+    $('#gh-user-data').html(
+        `<div id="loader">
+            <img src="assets/css/loader.gif" alt="loading..." />
+        </div>`
+    );
+
+    $.when(
+        $.getJSON(`https://api.github.com/users/${username}`)
+    )
+}
